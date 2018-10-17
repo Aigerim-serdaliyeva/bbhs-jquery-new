508f9b18d9519c115ef9422be7b899cb5bc591b2
Сарсенбаев Нурбол <kuma_official@mail.ru>
2018-10-17 13:25:19 +0600

    fix in mail.php

--- app/mail.php
+++ app/mail.php
@@ -40,7 +40,7 @@ if(isset($_POST["info"])) {
   'From: '.$project_name.' '.$server_mail. PHP_EOL .
   'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-  mail($admin_email, $form_subject, $message, $headers);
+  mail($admin_email, adopt($form_subject), $message, $headers);
 
   header("Location: /thanks.html");
   
